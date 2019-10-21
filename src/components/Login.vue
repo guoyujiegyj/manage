fb17fc747105bc21789cc83032121269bb54ad56
gyj <1401357358@qq.com>
2019-10-21 16:03:16 +0800

    登录页背景和登录框样式完成

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">legonn</div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+    .login_container{
+        height: 100%;
+        background: #2b4b6b;
+    }
+    .login_box{
+        width:350px;
+        height:200px;
+        background: #fff;
+        border-radius: 5px;
+        position:absolute;
+        left:50%;
+        top:50%;
+        transform: translate(-50%,-50%);
+    }
+</style>
