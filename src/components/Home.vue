baf89f0a98372a092b62c8facab85ffb29af630b
gyj <1401357358@qq.com>
2019-10-22 12:32:22 +0800

    header布局实现

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
+  <el-container class="container_box">
+    <!--头部-->
+    <el-header>
+      <div class="header_left">
+        <img src="../assets/heima.png" alt="">
+        <h2>后台管理系统</h2>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--主体-->
     <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
-      </el-container>
+      <!--侧边-->
+      <el-aside width="200px">Aside</el-aside>
+      <!--右边-->
+      <el-main>Main</el-main>
     </el-container>
-  </div>
+  </el-container>
 </template>
 <script>
 export default {
@@ -24,5 +31,32 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+  .el-header{
+    display: flex;
+    justify-content: space-between;
+    /*元素在垂直方向居中*/
+    align-items: center;
+    >.header_left{
+      display:flex;
+      align-items: center;
+      /*文字颜色*/
+      >h2{
+        color:#fff;
+        margin-left:20px;
+      }
+    }
+    //header 的颜色
+    background: #373d41;
+  }
+  .el-aside{
+    background: #333744;
+  }
+  .el-main{
+    background:#eaedf1;
+  }
+  .container_box{
+    height:100%;
+  }
+  
 </style>
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { 
+  Button, 
+  Form, 
+  FormItem, 
+  Input, 
+  Message,
+  Aside,
+  Container,
+  Header,
+  Main 
+} from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Aside)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
 
-Vue.prototype.$message=Message
+
+Vue.prototype.$message = Message
