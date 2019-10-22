f78907853268922bee43e1926620821cf13666aa
gyj <1401357358@qq.com>
2019-10-22 10:32:46 +0800

    退出功能完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>home</div>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 <script>
 export default {
-  
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear('token')
+      this.$router.push('/login')
+    }  
+  }
 }
 </script>
 <style lang="less" scoped>
