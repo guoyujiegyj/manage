e98cc584a1f78321fb83e1b96c864b19d1c90b99
gyj <1401357358@qq.com>
2019-10-22 10:23:05 +0800

    路由导航守卫判断token完成

--- src/router/index.js
+++ src/router/index.js
@@ -23,5 +23,15 @@ const routes = [
 const router = new VueRouter({
   routes
 })
-
+// 路由导航守卫，在页面跳转之前判断是否有本地token，
+router.beforeEach((to, from, next) => {
+  // 如果to.path是login，直接放行。
+  if(to.path==='/login') return next()
+  // 不是login，则判断是否有token
+  const token = window.sessionStorage.getItem('token')
+  // 如果没有token，回到login页。
+  if(!token) return next("/login")
+  // 否则说明有token，进行跳转。
+  next()
+})
 export default router
