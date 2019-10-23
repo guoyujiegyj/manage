4b44fb6a576c2798c5f83a885cca5f90320dd968
gyj <1401357358@qq.com>
2019-10-23 09:56:06 +0800

    用户分页完成

--- src/components/users/User.vue
+++ src/components/users/User.vue
@@ -32,21 +32,35 @@
         <el-table-column prop="role_name" label="角色" width="180"></el-table-column>
         <el-table-column prop="mg_state" label="状态" width="180">
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
-
-            </el-switch>
+            <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180">
-          <template >
+          <template>
             <el-button type="primary" size="mini" icon="el-icon-edit"></el-button>
-            <el-tooltip :enterable="false" class="item" effect="dark" content="修改角色" placement="top">
+            <el-tooltip
+              :enterable="false"
+              class="item"
+              effect="dark"
+              content="修改角色"
+              placement="top"
+            >
               <el-button type="warning" size="mini" icon="el-icon-setting"></el-button>
             </el-tooltip>
             <el-button type="danger" size="mini" icon="el-icon-delete"></el-button>
           </template>
         </el-table-column>
       </el-table>
+      <!--分页-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[2, 4, 6, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -58,10 +72,13 @@ export default {
       // 请求用户列表时携带的参数。
       queryInfo: {
         query: '',
-        pagenum: '1',
-        pagesize: '3'
+        // 页码
+        pagenum: 1,
+        // 每页的条数
+        pagesize: 3
       },
-      searchVal: ''
+      searchVal: '',
+      total: 0
     }
   },
   created() {
@@ -75,8 +92,22 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error(res.meta.msg)
       }
-      console.log(res)
+      this.total=res.data.total
       this.usersList = res.data.users
+    },
+    // 每页显示的条数。
+    handleSizeChange(val) {
+      // 获取到每页显示的条数，并赋值。
+      this.queryInfo.pagesize=val
+      // 然后重新请求数据。
+      this.getUsersList()
+    },
+    // 当前页码改变时触发
+    handleCurrentChange(val) {
+      // 逻辑：当页码改变时，获取到页码值，即val。将其赋给data的queryInfo的页码
+      // 然后重新发起数据请求，即可获得数据。
+      this.queryInfo.pagenum=val
+      this.getUsersList()
     }
   }
 }
@@ -84,5 +115,8 @@ export default {
 <style lang="less" scoped>
 .el-card {
   margin-top: 10px;
+}
+.el-pagination{
+  margin-top:10px;
 }
 </style>
