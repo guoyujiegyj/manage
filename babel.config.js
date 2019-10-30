f8df831140eaf0d77b103c0adb02d7988fe2d917
gyj <1401357358@qq.com>
2019-10-30 16:21:22 +0800

    上线阶段和开发阶段，配置文件。

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+// 项目开发阶段需要的 babel插件
+const devBabel = []
+if(process.env.NODE_ENV=== 'production'){
+  devBabel.push('transform-undefined-to-void')
+}
 module.exports = {
   'presets': [
     '@vue/cli-plugin-babel/preset'
@@ -10,6 +15,8 @@ module.exports = {
         'styleLibraryName': 'theme-chalk'
       }
     ],
-    'transform-undefined-to-void'
+    // 打包时需要移除所有的console
+    ...devBabel
   ]
+  
 }
